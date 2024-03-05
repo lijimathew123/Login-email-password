00d6e85cde398bdb2df7fc38bd1343f02a13db1f
Liji Mathew <107242364+lijimathew123@users.noreply.github.com>
2024-03-05 12:47:11 +0530

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UserRegistrationAPIView, UserLoginAPIView
+from .views import UserRegistrationAPIView, UserLoginAPIView,UserProfileAPIView,UserProfileUpdateAPIView
 
 urlpatterns = [
     # your other urlpatterns here
     path('api/register/', UserRegistrationAPIView.as_view(), name='user-registration'),
     path('api/login/', UserLoginAPIView.as_view(), name='user-login'),
     path('api/profile/', UserProfileAPIView.as_view(), name='user-profile'),
+    path('api/profile/update', UserProfileUpdateAPIView.as_view(), name='user-update-view'),
 ]
